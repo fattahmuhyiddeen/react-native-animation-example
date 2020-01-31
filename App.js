76decb930e4d51ed622294fe7f57f6ecb9e50549
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2020-01-31 19:11:19 +0800

    bug fix

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import ShareTimeline from './examples/ShareTimeline';
 import Loop from './examples/Loop';
 import List from './examples/List';
 import List2 from './examples/List2';
-
+import DragDrop from './examples/DragDrop';
+import ScrollViewHeader from './examples/ScrollViewHeader';
+import DragDrop2 from './examples/DragDrop2';
 
 export default () => (
   <SafeAreaView style={{ flex: 1 }}>
     {/* <ShareTimeline /> */}
     {/* <Loop /> */}
     {/* <List /> */}
-    <List2 />
+    {/* <List2 /> */}
+    {/* <DragDrop /> */}
+    <ScrollViewHeader />
+    {/* <DragDrop2 /> */}
   </SafeAreaView>
 );
---
+++ examples/DragDrop2.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { Animated, PanResponder } from 'react-native';
+const timeline = new Animated.ValueXY();
+const panResponder = PanResponder.create({
+  onStartShouldSetPanResponder: () => true,
+  onPanResponderMove: Animated.event([null, {
+    dx: timeline.x,
+    dy: timeline.y
+  }]),
+  onPanResponderRelease: (e, gesture) => {
+    Animated.spring(
+      timeline,
+      { toValue: { x: 0, y: 0 } }
+    ).start();
+  }
+});
+
+export default () => {
+  return (
+    <Animated.View
+      {...panResponder.panHandlers}
+      style={[timeline.getLayout(), { height: 50, width: 50, backgroundColor: 'red' }]} />
+  );
+};
