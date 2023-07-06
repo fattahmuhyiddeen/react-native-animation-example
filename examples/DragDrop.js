bb30b1b8bf1b9b1597675ce7555fffb7b11b55ff
Fattah Muhyiddeen <fattahmr@m1.com.sg>
2023-07-06 17:26:40 +0800

    modify drag drop

--- examples/DragDrop.js
+++ examples/DragDrop.js
@@ -1,26 +1,36 @@
-import React, {useEffect} from 'react';
+import React, {useRef} from 'react';
 import {Animated, PanResponder} from 'react-native';
-const timeline = new Animated.ValueXY();
-const panResponder = PanResponder.create({
-  onStartShouldSetPanResponder: () => true,
-  onPanResponderMove: Animated.event([
-    null,
-    {
-      dx: timeline.x,
-      dy: timeline.y,
-    },
-  ]),
-  onPanResponderRelease: (e, gesture) => {
-    Animated.spring(timeline, {toValue: {x: 0, y: 0}}).start();
-  },
-});
 
 export default () => {
+  const timeline = useRef(new Animated.ValueXY());
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: timeline.current.x,
+          dy: timeline.current.y,
+        },
+      ],
+      {
+        useNativeDriver: false, // PanResponder doesn't support native driver yet
+      },
+    ),
+    onPanResponderRelease: (e, gesture) => {
+      Animated.spring(timeline.current, {
+        useNativeDriver: false, // cannot use native driver because using un supported style prop
+        toValue: {x: 0, y: 0},
+      }).start();
+    },
+  });
+
   return (
     <Animated.View
       {...panResponder.panHandlers}
       style={[
-        timeline.getLayout(),
+        timeline.current.getLayout(),
         {height: 50, width: 50, backgroundColor: 'red'},
       ]}
     />
--- examples/DragDrop2.js
+++ examples/DragDrop2.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {Animated, PanResponder} from 'react-native';
-const timeline = new Animated.ValueXY();
-let coordinate = {x: 0, y: 0};
-timeline.addListener(v => (coordinate = v));
-const panResponder = PanResponder.create({
-  onStartShouldSetPanResponder: () => true,
-  onPanResponderGrant: () => {
-    timeline.setOffset({x: coordinate.x, y: coordinate.y});
-  },
-  onPanResponderMove: Animated.event([
-    null,
-    {
-      dx: timeline.x,
-      dy: timeline.y,
-    },
-  ]),
-});
 
-export default () => (
-  <Animated.View
-    {...panResponder.panHandlers}
-    style={[
-      timeline.getLayout(),
-      {height: 50, width: 50, backgroundColor: 'red'},
-    ]}
-  />
-);
+export default () => {
+  const timeline = useRef(new Animated.ValueXY());
+  const coordinate = useRef({x: 0, y: 0});
+  useEffect(() => {
+    timeline.current.addListener(v => (coordinate.current = v));
+  }, []);
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      timeline.current.setOffset({
+        x: coordinate.current.x,
+        y: coordinate.current.y,
+      });
+    },
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: timeline.current.x,
+          dy: timeline.current.y,
+        },
+      ],
+      {
+        useNativeDriver: false, // PanResponder doesn't support native driver yet
+      },
+    ),
+  });
+
+  return (
+    <Animated.View
+      {...panResponder.panHandlers}
+      style={[
+        timeline.current.getLayout(),
+        {height: 50, width: 50, backgroundColor: 'red'},
+      ]}
+    />
+  );
+};
